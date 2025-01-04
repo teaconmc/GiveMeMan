f7a296d7ba90d269ff448dbbf597242de8c27804
zbx1425 <support@zbx1425.cn>
2025-01-04 16:37:36 +0800

    Eat the backpack with WrapOperation

--- src/main/java/org/teacon/gimmeman/mixin/L2BackpackMixin.java
+++ src/main/java/org/teacon/gimmeman/mixin/L2BackpackMixin.java
@@ -1,24 +1,27 @@
 package org.teacon.gimmeman.mixin;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.xkmc.l2backpack.events.StartUpGiveItemEvents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.teacon.gimmeman.GiveMeMan;
 
 @Mixin(StartUpGiveItemEvents.class)
 public class L2BackpackMixin {
 
-    @Inject(method = "onPlayerTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;placeItemBackInInventory(Lnet/minecraft/world/item/ItemStack;)V", shift = At.Shift.AFTER))
-    private static void afterBackpackGiven(PlayerTickEvent.Post event, CallbackInfo ci) {
+    @WrapOperation(method = "onPlayerTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;placeItemBackInInventory(Lnet/minecraft/world/item/ItemStack;)V"))
+    private static void beforeBackpackGiven(Inventory instance, ItemStack arg, Operation<Void> original, PlayerTickEvent.Post event) {
         if (!(event.getEntity() instanceof ServerPlayer player)) return;
         LootTable lootTable = player.server.reloadableRegistries().getLootTable(
                 ResourceKey.create(Registries.LOOT_TABLE, GiveMeMan.id("startup_items")));
